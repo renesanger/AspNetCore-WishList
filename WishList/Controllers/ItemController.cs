fb7af49c607d1a55d8e1dfe570d59cd6085e2aee
renesanger <renejsanger@gmail.com>
2021-12-22 12:48:48 -0500

    Create Item Controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext ctx)
+        {
+            ctx = _context;
+        }
+        public IActionResult Index()
+        {
+            List<Item> list_items = new List<Item>();
+            foreach (var item in _context.Items)
+            {
+                list_items.Add(item);
+            }
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item item = _context.Items.Find(Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
