26a65a9a8419d51c0866b9061ea5f1d3f58fe81a
renesanger <renejsanger@gmail.com>
2021-12-22 12:56:52 -0500

    bug fixes

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -38,7 +38,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            Item item = _context.Items.Find(Id);
+            Item item = _context.Items.FirstOrDefault(e => e.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
 
